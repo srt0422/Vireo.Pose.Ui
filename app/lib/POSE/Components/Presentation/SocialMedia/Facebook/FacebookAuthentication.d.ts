df3843b71c6b5d7b4cf68a96ae44713e6ba12836
Scott Terry <scottterry@Scotts-MBP.fios-router.home>
2017-02-25 15:49:18 -0800

    removed social auth and facebook auth components because they're unused

--- app/lib/POSE/Components/Presentation/SocialMedia/Facebook/FacebookAuthentication.d.ts
+++
@@ -1,7 +0,0 @@
-import AuthBase from "../AuthBase";
-declare class FacebookAuthentication extends AuthBase {
-    render(): JSX.Element;
-    componentDidMount(): void;
-    ensureLoggedIn(): void;
-}
-export = FacebookAuthentication;
--- app/lib/POSE/Components/Presentation/SocialMedia/socialauth.d.ts
+++
@@ -1,8 +0,0 @@
-import React = require("react");
-declare class SocialAuth extends React.Component<any, any> {
-    authenticateFacebook: boolean;
-    authenticateTwitter: boolean;
-    render(): JSX.Element;
-    componentWillMount(): boolean;
-}
-export = SocialAuth;
