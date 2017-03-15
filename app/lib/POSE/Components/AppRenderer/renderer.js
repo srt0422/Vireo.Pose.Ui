a55d7c15f36d4e21a160ad13abb346a09402aac8
Scott Terry <scottterry@Scotts-MBP.fios-router.home>
2017-03-14 20:22:50 -0700

    update

---
+++ app/lib/POSE/Components/AppRenderer/renderer.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import BootstrapContainerLayout from "../../../HTML/Components/BootstrapContainerLayout";
+import FacebookSdk from "../Presentation/SocialMedia/Facebook/FacebookSdk";
+import Poser from "../Presentation/Poser";
+export function renderer() {
+    return (<div>
+            <FacebookSdk appId="764673526884567"></FacebookSdk>
+
+            <BootstrapContainerLayout>
+                {this.props.children || (<Poser></Poser>)}
+            </BootstrapContainerLayout>
+        </div>);
+}
+export default renderer;
