65639c8eb946fb3209f7dfad566000955dcf8131
Scott Terry <scottterry@Scotts-MBP.fios-router.home>
2017-04-09 19:08:13 -0700

    update

--- mocha-babel.js
+++ mocha-babel.js
@@ -1,4 +1,4 @@
 ﻿require('babel-core/register')({
-    presets: ["es2015", "react", "stage-0"],
+    presets: ["es2015", "react", "stage-0", "node6"],
     plugins: ['transform-runtime', 'transform-async-to-generator']
 });
