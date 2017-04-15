f06612cdb8bb8072580570ae3799adf45ca9bf46
Scott Terry <scottterry@Scotts-MBP.fios-router.home>
2017-04-14 20:26:51 -0700

    installed codepush

--- android/app/src/main/java/com/poseui/MainApplication.java
+++ android/app/src/main/java/com/poseui/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.codepush.react.CodePush;
 import io.fullstack.oauth.OAuthManagerPackage;
 import io.fullstack.firestack.FirestackPackage;
 import ga.piroro.rnt.RNTPackage;
@@ -23,6 +24,12 @@ import java.util.List;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+    @Override
+    protected String getJSBundleFile() {
+      return CodePush.getJSBundleFile();
+    }
+
     @Override
     protected boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -32,6 +39,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new CodePush(null, getApplicationContext(), BuildConfig.DEBUG),
             new OAuthManagerPackage(),
             new FirestackPackage(),
             new RNTPackage(),
